8deb47b08dd3959abc5dff90f0b0fd8851ac2e1b
RC Maples <rc@rcmaples.io>
2024-10-03 14:48:21 -0400

    capture is working

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import { init as initFullStory } from '@fullstory/browser';
+// import { useEffect } from 'react';
+// import { init as initFullStory } from '@fullstory/browser';
 
 import '../globals.css';
 
@@ -10,11 +10,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    initFullStory({
-      orgId: 'o-1X4FZW-na1',
-    });
-  }, []);
+  // useEffect(() => {
+  //   initFullStory({
+  //     orgId: 'o-1X4FZW-na1',
+  //   });
+  // }, []);
   return (
     <html lang="en">
       <body className="bg-white min-h-screen">{children}</body>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 // import './globals.css';
+'use client';
 
 import '@/app/globals.css';
+
+import { useEffect } from 'react';
+import { init as initFullStory } from '@fullstory/browser';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    initFullStory({
+      orgId: 'o-1X4FZW-na1',
+    });
+  }, []);
+
   return (
     <html lang="en">
       <body>{children}</body>
